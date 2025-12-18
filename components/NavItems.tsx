a1decc52925beb53053d9bddb3f02aa877ad0136
Anna Baharyan <anna.baharyan@gohacktech.com>
2025-12-18 13:26:48 +0400

    create header

---
+++ components/NavItems.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { NAV_ITEMS } from '@/lib/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const NavItems = () => {
+  const pathName = usePathname();
+
+  const isActive = (path: string) => {
+    if (path === '/') return pathName === '/';
+    return pathName.startsWith(path);
+  };
+
+  return (
+    <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
+      {NAV_ITEMS.map(({ link, title }) => (
+        <li key={link}>
+          <Link
+            href={link}
+            className={`hover:text-yellow-500 transition-colors ${isActive(link) ? 'text-gray-100' : ''}`}
+          >
+            {title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default NavItems;
---
+++ prettier.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  $schema: 'https://json.schemastore.org/prettierrc',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  useTabs: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
